a941e9ab35ee7bf3f87641a0083380efdc547d3c
da7id <da7idbarron@me.com>
2015-11-03 16:53:13 -0600

    Add router to add new beans selection to go to add new beans template

--- client/templates/pours/pour_cup.js
+++ client/templates/pours/pour_cup.js
@@ -1,7 +1,15 @@
 Template.pourCup.rendered=function() {
   $('#brew-datepicker').datepicker({
     todayBtn: "linked",
-    clearBtn: true,
+    clearBtn: trjue,
     todayHighlight: true
   });
+  console.log("TEST");
 }
+
+Template.pourCup.events({
+  "change #beans": function(e){
+    if($('#beans option:selected').val()==="add_new")
+      Router.go('addBeans'); 
+  }
+})
