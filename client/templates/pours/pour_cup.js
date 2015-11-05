bda3d5ae110eb246fb62a2e7f228ff5ba035b9b1
da7id <da7idbarron@me.com>
2015-11-05 13:56:59 -0600

    Add method to pour cups into collection

--- client/templates/pours/pour_cup.js
+++ client/templates/pours/pour_cup.js
@@ -4,15 +4,33 @@ Template.pourCup.rendered=function() {
     clearBtn: true,
     todayHighlight: true
   });
-  console.log("TEST");
 }
 
 Template.pourCup.events({
-  "change #beans": function(e){
+  'change #beans': function(e) {
     if($('#beans option:selected').val()==="add_new")
       Router.go('addBeans'); 
+  },
+
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var pour = {
+      brewDate: $(e.target).find('[name=brew_date]').val(),
+      brewTime: $(e.target).find('[name=time_taken]').val(),
+      brew:$(e.target).find('[name=brew]').val(),
+      temperature: $(e.target).find('[name=temperature]').val(),
+      beans: $(e.target).find('[name=beans]').val(),
+      grind: $(e.target).find('[name=grind]').val()
+    };
+
+    Meteor.call('pourInsert', pour, function(error, result) {
+      if(error)
+        return alert(error.reason);
+      Router.go('pourList');
+    });
   }
-})
+});
 
 Template.pourCup.helpers({
   beans: function() {
