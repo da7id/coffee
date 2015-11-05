d66cfb5cb152202a89183c7958f00a5293a29f0b
da7id <da7idbarron@me.com>
2015-11-05 13:25:24 -0600

    Add more sample data entries

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,6 +1,29 @@
 if( Pours.find().count() === 0) {
   Pours.insert({
     brew: 'Hario v60',
+    time_taken: '3:20',
+    brew_date: Date(),
+    temperature: '60',
+    beans: 'Starbucks Sumatra',
+    grind: '5',
+    date: Date()
+  });
+  Pours.insert({
+    brew: 'Hario v60',
+    time_taken: '3:20',
+    brew_date: Date(),
+    temperature: '60',
+    beans: 'Starbucks Sumatra',
+    grind: '5',
+    date: Date()
+  });
+  Pours.insert({
+    brew: 'Hario v60',
+    time_taken: '3:20',
+    brew_date: Date(),
+    temperature: '60',
+    beans: 'Starbucks Sumatra',
+    grind: '5',
     date: Date()
   });
 }
@@ -8,5 +31,13 @@ if( Beans.find().count() === 0) {
   Beans.insert({
     brewer: 'Starbucks',
     roast: 'Sumatra' 
+  });
+  Beans.insert({
+    brewer: 'Other Brewer',
+    roast: 'Sumatra' 
+  });
+  Beans.insert({
+    brewer: 'Starbucks',
+    roast: 'French Roast' 
   });
 }
