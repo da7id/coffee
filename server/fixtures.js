e471179a497d74c39478bd07d2b2c4f2b8744a52
da7id <da7idbarron@me.com>
2015-11-11 21:41:46 -0600

    Add 20 randomized pours

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,37 +1,26 @@
+sample_brews=['Hario v60','Chemex','French Press'];
+sample_times=['11:30','9:14','3:34','12:20'];
+sample_dates=['10/4/2015','9/30/2015','4/28/2015','3/12/2015','10/1/2015'];
+sample_temperatures=['200','194','180'];
+sample_beans=['Starbucks Sumatra','Starbucks French Roast','Big Bend Blonde'];
+sample_grind=['1','6','9'];
+sample_rating=['3','4','5'];
+sample_notes=['This coffee ruled','This coffee was lame','This coffee was totally mediocre'];
+
 if( Pours.find().count() === 0) {
-  Pours.insert({
-    brew: 'Hario v60',
-    brewTime: '3:20',
-    brewDate: '11/4/2015',
-    temperature: '60',
-    beans: 'Starbucks Sumatra',
-    grind: '5',
-    date: Date(),
-    rating: '3',
-    notes: 'Sample notes'
-  });
-  Pours.insert({
-    brew: 'Hario v60',
-    brewTime: '3:20',
-    brewDate: '11/4/2015',
-    temperature: '60',
-    beans: 'Starbucks Sumatra',
-    grind: '5',
-    date: Date(),
-    rating: '3',
-    notes: 'Sample notes'
-  });
-  Pours.insert({
-    brew: 'Hario v60',
-    brewTime: '3:20',
-    brewDate: '11/4/2015',
-    temperature: '60',
-    beans: 'Starbucks Sumatra',
-    grind: '5',
-    date: Date(),
-    rating: '3',
-    notes: 'Sample notes'
-  });
+  for (i = 0; i < 20; i++) {
+    Pours.insert({
+      brew: sample_brews[Math.floor((Math.random() * 3))],
+      brewTime: sample_times[Math.floor((Math.random() * 3))],
+      brewDate: sample_dates[Math.floor((Math.random() * 5))],
+      temperature: sample_temperatures[Math.floor((Math.random() * 3))],
+      beans: sample_beans[Math.floor((Math.random() * 3))],
+      grind: sample_grind[Math.floor((Math.random() * 3))],
+      date: sample_dates[Math.floor((Math.random() * 3))],
+      rating: sample_rating[Math.floor((Math.random() * 3))],
+      notes: sample_notes[Math.floor((Math.random() * 3))]
+    });
+  }
 }
 if( Beans.find().count() === 0) {
   Beans.insert({
